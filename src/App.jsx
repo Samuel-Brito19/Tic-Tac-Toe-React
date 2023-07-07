19ec2b6a0463dd2d0cf30ac67e4469d1b79491ef
Samuel Brito <sambriton7@gmail.com>
2023-07-07 13:33:55 -0300

    Starting the javascript logic

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,27 @@
+import { useState } from "react"
 
 function App() {
 
+  const [table, setTable] = useState(Array(9).fill(""))
+  const [player, setPlayer] = useState('X')
+
+  const handlePlayerClick = (index) => {
+    if (table[index] === "") {
+      const newTable = [...table]
+      newTable[index] = player
+      setTable(newTable)
+      setPlayer(player === 'X' ? 'O' : 'X')
+    }
+  }
 
   return (
-    <h1>Hello niggas!</h1>
+    <div className="tabuleiro">
+      {table.map((value, index) => (
+        <div key={index}
+          className="celula"
+          onClick={() => handlePlayerClick(index)}>{value}</div>
+      ))}
+    </div>
   )
 }
 

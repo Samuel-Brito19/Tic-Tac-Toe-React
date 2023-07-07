eb3d923518f86498a27c56b2a6de236878d7a4ea
Samuel Brito <sambriton7@gmail.com>
2023-07-07 20:10:46 -0300

    Puting the winner logic with useEffect

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,28 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 
 function App() {
 
   const [table, setTable] = useState(Array(9).fill(""))
   const [player, setPlayer] = useState('X')
   const [winner, setWinner] = useState(null)
+
+  useEffect(() => {
+    const winnerLines = [
+      [0, 1, 2], [3, 4, 5], [6, 7, 8],
+      [0, 3, 6], [1, 4, 7], [2, 5, 8],
+      [0, 4, 8], [2, 4, 6]
+    ];
+
+    //N entendi
+    for (let i = 0; i < winnerLines.length; i++) {
+      const [a, b, c] = winnerLines[i]
+      if (table[a] !== "" && table[a] === table[b] &&
+        table[b] === table[c]) {
+        setWinner(table[a])
+        break;
+      }
+    }
+  }, [table])
 
   const handlePlayerClick = (index) => {
     if (table[index] === "") {
@@ -16,13 +34,22 @@ function App() {
   }
 
   return (
-    <div className="tabuleiro">
-      {table.map((value, index) => (
-        <div key={index}
-          className="celula"
-          onClick={() => handlePlayerClick(index)}>{value}</div>
-      ))}
-    </div>
+    <>
+      <div className="tabuleiro">
+        {table.map((value, index) => (
+          <div
+            key={index}
+            className='celula'
+            onClick={() => handlePlayerClick(index)}>{value}</div>
+        ))}
+      </div>
+
+      {winner && (
+        <div className="winner">
+          The player {winner} wins!
+        </div>
+      )}
+    </>
   )
 }
 
